64d66e203979f9833375ab2c9b17723e60a46d5c
mx-zzz <125172061+mx-zzz@users.noreply.github.com>
2024-01-10 13:06:30 +1100

    Update PriceDataGenerator.py

--- PriceDataGenerator.py
+++ PriceDataGenerator.py
@@ -7,35 +7,14 @@ from SampleSizeError import SampleSizeError
 import pandas_market_calendars as mcal
 from datetime import datetime
 from sklearn.preprocessing import MinMaxScaler
-
-
-# Enter Stock Tickers here.
-# Note: Only works for NYSE and NASDAQ Listed Stock Tickers.
+import os
 
 
 
-stock_names = ["ADSK", "ALGN", "ALNY", "ALTR", "AMTD", "AMZN", "ANSS", "AON", "APA", "APD", "APH", "ARG",
-"ASML", "ATVI", "AVGO", "BIDU", "BIIB", "BKNG", "BMRN", "CDNS", "CDW", "CERN", "CHKP", "CHTR", "CMCSA", "COST",
-"CPRT", "CSCO", "CSGP", "CSX", "CTAS", "CTRP", "CTXS", "DLTR", "DOCU", "DXCM", "EA", "EBAY", "ESRX", "EXC", "EXPE",
-"FAST", "FB", "FISV", "FOX", "FOXA", "GILD", "GOOG", "GOOGL", "IDXX", "ILMN", "INCY", "INTC", "INTU", "ISRG", "JD",
-"KDP", "KHC", "KLAC", "LBTYA", "LBTYB", "LBTYK", "LULU", "MAR", "MCHP", "MDLZ", "MELI", "MNST", "MSFT", "MTCH", "MU",
-"MXIM", "NLOK", "NTAP", "NTES", "NVDA", "NXPI", "ORLY", "PAYX", "PCAR", "PDD", "PEP", "PYPL", "QCOM", "REGN", "ROST",
-"SBUX", "SGEN", "SIRI", "SNPS", "SPLK", "SWKS", "TCOM", "TMUS", "TSLA", "TTWO", "TXN", "UAL", "VRSK", "VRSN", "VRTX",
-"WBA", "WDAY", "WDC", "XEL", "XLNX", "ZM","AAPL", "MSFT", "AMZN", "GOOGL", "FB", "INTC", "CSCO", "CMCSA", "PEP", "AMGN", "COST", "NFLX", "ADBE",
-"MDLZ", "PYPL", "SBUX", "GILD", "TMUS", "TXN", "QCOM", "BIDU", "AVGO", "NVDA", "ADP", "VRTX", "ATVI", "MU", "INTU",
-"MELI", "CERN", "CTSH", "CSX", "DLTR", "EXPD", "FAST", "FISV", "HSIC", "IDXX", "JBHT", "CHKP", "CTAS", "ADI", "EXPE",
-"ROST", "WDC", "ALXN", "AMAT", "BBBY", "BIIB", "CA", "CELG", "CTXS", "DISCA", "DISH", "EBAY", "ESRX", "FOXA", "GOOG",
-"HAS", "HOLX", "ILMN", "INCY", "ISRG", "LBTYA", "LRCX", "MAR", "MCHP", "MYL", "NLOK", "NTAP", "ORLY", "PCAR", "PCLN",
-"REGN", "SHPG", "SIRI", "STX", "SYMC", "TSLA", "VIAB", "VOD", "WBA", "XRAY", "YHOO", "ZION", "KLAC", "SGEN", "SPLK",
-"TTWO", "ULTA", "VRSK", "VRSN", "WFM", "XLNX"]
-
-index_offset = 251
-
+index_offset = 0
 
 start_date = "2011-06-01"
 end_date = "2017-06-01"
-
-
 
 invalid_tickers = []
 
@@ -48,21 +27,24 @@ def get_trading_days(start_date, end_date):
 
     trading_days = nasdaq.valid_days(start_date=start_date, end_date=end_date)
 
-    return len(trading_days)-4
+    return len(trading_days)
 
 
-def check_training_data(index, name,start_date, end_date):
-    rows = get_trading_days(start_date,end_date)
+def check_training_data(file_path, name, rows):
+
     columns = 7
-    file_path = "TrainingData\\" + str(index) + ".csv"
-
+    error = False
     with open(file_path, 'r') as file:
         lines = 0
         trainingdata = csv.reader(file)
         lines = sum(1 for line in trainingdata)  # Count the number of lines
-        if lines < 700:
-            invalid_tickers.append(name)
-            raise SampleSizeError(f"Wrong Amount of lines in TrainingData\nShould be {rows}\nWas Instead: {lines}\n Ticker Name: {name}")
+        try:
+            if lines != rows:
+                raise SampleSizeError(f"Wrong Amount of lines in TrainingData\nShould be {rows}\nWas Instead: {lines}\n Ticker Name: {name}")
+        except SampleSizeError:
+            error = True
+
+
 
     with open(file_path, 'r') as trainingdata_file:
         trainingdata = csv.reader(trainingdata_file)
@@ -71,118 +53,154 @@ def check_training_data(index, name,start_date, end_date):
                 row = next(trainingdata)
                 column_count = len(row)
                 if columns != column_count:
-                    invalid_tickers.append(name)
-                    raise SampleSizeError(f"Wrong Amount of Columns in TrainingData\nShould be {columns}\nWas Instead: {column_count}\nFile Name: {str(index)}\n Ticker Name: {name}")
+                    error = True
+                    raise SampleSizeError(f"Wrong Amount of Columns in TrainingData\nShould be {columns}\nWas Instead: {column_count}\nFile Name: {file_path}\n Ticker Name: {name}")
             except StopIteration:
-                break  # or do something else if you want
-
-def normalize_training_data(filenum) :
-    data = pd.read_csv("TrainingData\\" + filenum + ".csv", skiprows=1)
-    scaler = MinMaxScaler()
-
-    data_normalized = data.apply(lambda x: scaler.fit_transform(x.values.reshape(-1, 1)).flatten() if x.name in data else x)
-    data_normalized.to_csv("TrainingData\\" + filenum + ".csv", index=False, quoting=csv.QUOTE_NONE)
-    data_normalized = pd.read_csv("TrainingData\\" + filenum + ".csv", skiprows=2)
-    data_normalized.to_csv("TrainingData\\" + filenum + ".csv", index=False, quoting=csv.QUOTE_NONE)
+                break
+    if(error == True) :
+        invalid_tickers.append(file_path)
+    else :
+        print(f"Ticker {name} Successfully Generated ")
 
 
+def normalize_training_data(file_path):
 
-def init_training_data(index,stock_names,name, start_date, end_date):
+    print(f"Normalizing data from {file_path}")
 
+    try:
+        data = pd.read_csv(file_path)
+        print("Data after reading CSV:")
+        print(data.head())  # Print first few lines of the data
+
+        scaler = MinMaxScaler()
+        data_normalized = data.apply(
+            lambda x: scaler.fit_transform(x.values.reshape(-1, 1)).flatten() if x.name in data else x)
+        data_normalized.to_csv(file_path, index=False, quoting=csv.QUOTE_NONE)
+
+        # Additional debug: Check the data after normalization
+        print("Data after normalization:")
+        print(data_normalized.head())
+
+    except(ValueError) as e:
+        print(f"Error while normalizing data in file {file_path}: {e}")
+        print(data.to_string())
+
+
+def init_training_data(index,file_path, name, start_date, end_date,num_of_rows) :
     try:
         stock = yf.Ticker(name)
         data = stock.history(start=start_date, end=end_date)
+        data_frame = pd.DataFrame(data)
+    except Exception as e:
+        raise InvalidDataError(f"No Data found for ticker : {name} in {file_path}")
+        invalid_tickers.append(file_path)
+        return
 
-        #print(data)
+    try:
+        if data.empty:
+            raise InvalidDataError(f"No data fetched for {name}")
+            invalid_tickers.append(file_path)
+            return
 
-        # Estimate revenue by multiplying Close price with Volume
-        data['Revenue'] = data['Close'] * data['Volume']
+        if data_frame.to_string().__contains__(".0.") :
+            data_frame.replace("0.0.1","0.0",inplace=True)
+            print(f"Data recieved from API contians invalid symbols but has been restored.")
+            if data_frame.to_string().__contains__(".0.") :
+                raise InvalidDataError(f"Data recieved from API contians invalid symbols.")
+                return
 
-        # Group the data by week and sum the revenue
-        revenue_by_week = data.groupby(pd.Grouper(freq='W'))['Revenue'].sum()
+        print(f"Writing data to {file_path}")
+        data_frame.to_csv(file_path, index=False, quoting=csv.QUOTE_NONE)
 
-        # Reset the index to remove the default index
-        revenue_by_week = revenue_by_week.reset_index()
+        # Debug: Print the first few lines of the data
+        print("Data preview:", data.head())
 
-        # Merge weekly revenue data with daily price data based on the nearest week start date
-        data['Week_Start'] = data.index - pd.offsets.Week(weekday=0)
-        data = data.merge(revenue_by_week, how='left', left_on='Week_Start', right_on='Date', suffixes=('', '_Weekly'))
+        normalize_training_data(file_path)
 
-        # Fill missing weekly revenue values forward
-        data['Revenue_Weekly'].fillna(method='ffill', inplace=True)
-
-
-        # Remove unnecessary columns
-        del data['Week_Start']
-        del data['Revenue_Weekly']
-        #data.drop(['Stock Splits'], axis=1, inplace=True)
-        del data['Stock Splits']
-
-        del data['Date']
-
-        # Save the price history to a CSV file
-        index_str = str(index)
-        file_path = "TrainingData\\" + index_str + ".csv"
-
-        data.to_csv(file_path, index=False, quoting=csv.QUOTE_NONE)
-
-        # Remove trailing comma at the end of each row in the CSV file
-        with open("TrainingData\\" + index_str + ".csv", 'r') as file:
-            lines = file.readlines()
-        with open("TrainingData\\" + index_str + ".csv", 'w', newline='') as file:
-            writer = csv.writer(file)
-            for line in lines[1:]:
-                if line.strip():  # Remove empty lines
-                    writer.writerow(line.strip().split(','))
-
-        normalize_training_data(index_str)
-
-
-
+        check_training_data(file_path, name, num_of_rows)
 
     except TypeError as e:
-
         raise InvalidDataError(f"Cannot Get Data for {name}, Symbol is likely delisted or there is no data for API to retrieve")
+        return
+    except InvalidDataError as e:
+        invalid_tickers.append(file_path)
+        print(e)
+        return
+
 
 
 def generate_training_data(stock_names,start_date,end_date,index_offset):
+
+    num_of_rows = get_trading_days(start_date,end_date)
+
     for name in stock_names:
         index = index_offset + stock_names.index(name)
-        try:
-            init_training_data(index,stock_names,name, start_date, end_date)
-            check_training_data(int(index), name,start_date, end_date)
-        except SampleSizeError as e:
-            print("")
-            print("Sample Size Error", e)
-            print("")
-            invalid_tickers.append(name)
-            stock_names.remove(name)
-        except InvalidDataError as e:
-            invalid_tickers.append(name)
-            print("")
-            print("Invalid Data Error", e)
-            print("")
+        file_path = "TrainingData\\" + str(index) + ".csv"
+        init_training_data(index,file_path,name, start_date, end_date,num_of_rows)
 
-        else:
-            print("")
+
 
     if len(invalid_tickers) > 0:
         print("The Following Tickers are invalid: ", invalid_tickers)
+        remove_tickers_prompt = input("Would you like to remove invalid tickers and Try again (Y/N) ")
+
+        if remove_tickers_prompt.strip() == "Y" :
+            print("Removing Invalid Tickers")
+            remove_invalid_tickers()
+            print("These are the Valid Tickers : ", stock_names)
+            print("Regenerating Data set with valid tickers")
+            rename_training_data()
+
+
     else:
         print("All data correctly saved and Valid")
 
+def remove_invalid_tickers():
+    for ticker in stock_names :
+        if ticker in invalid_tickers :
+            stock_names.remove(ticker)
 
+
+def rename_training_data():
+    base_path = "C:\\Users\\byzan\\PycharmProjects\\pythonProject\\"
+    training_data_path = os.path.join(base_path, "TrainingData")
+
+    # Remove invalid tickers
+    for filename in invalid_tickers:
+        file_path = os.path.join(base_path, filename)
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            print(f"File {file_path} has been deleted.")
+        else:
+            print(f"The file {file_path} does not exist.")
+
+    # Rename files in TrainingData
+    files = sorted(os.listdir(training_data_path), key=lambda x: int(x.split('.')[0]))
+    for index, data_file in enumerate(files):
+        current_file_path = os.path.join(training_data_path, data_file)
+        new_file_name = f"{index}.csv"
+        new_file_path = os.path.join(training_data_path, new_file_name)
+
+        if data_file != new_file_name:
+            os.rename(current_file_path, new_file_path)
+            print(f"Renamed {data_file} to {new_file_name}")
+
+def get_spx_tickers():
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    tables = pd.read_html(url)
+    spx_table = tables[0]
+    tickers = spx_table['Symbol'].tolist()
+    return tickers
 
 def main():
-    generate_training_data(stock_names, start_date, end_date,index_offset)
-
-
-
-
+    spx_tickers = get_spx_tickers()
+    generate_training_data(spx_tickers, start_date, end_date,index_offset)
 
 
 if __name__ == "__main__":
     main()
+
+
 
 
 
